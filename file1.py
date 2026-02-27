4aa6704dae3c083fb1e37f6911b35a83a8cf55a1
Shtirmann <103894479+Shtirmann@users.noreply.github.com>
2026-02-27 11:20:19 +0000

    docs(osa): auto-generate missing docstrings

--- file1.py
+++ file1.py
@@ -1,2 +1,12 @@
 def mul(a, b):
+    """
+    Calculates the product of two numbers.
+    
+    Parameters:
+    - a: the first number
+    - b: the second number
+    
+    Returns:
+    the product of a and b
+    """
   return a * b
