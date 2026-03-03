edc750b6dfeb182c55efdd1644179d05cf9cd690
Shtirmann <103894479+Shtirmann@users.noreply.github.com>
2026-03-03 08:34:58 +0000

    docs(osa): auto-generate missing docstrings

--- file2.py
+++ file2.py
@@ -1,2 +1,12 @@
 def mult(a, b):
+    """
+    Multiplies two numbers.
+    
+    Args:
+        a: The first number to be multiplied.
+        b: The second number to be multiplied.
+    
+    Returns:
+        The product of the two numbers.
+    """
   return a * b
