44b86faf6e19dec4dbfbeb743a6232edb047f10f
Shtirmann <103894479+Shtirmann@users.noreply.github.com>
2026-03-27 10:17:04 +0000

    docs(osa): auto-generate missing docstrings

--- file1.py
+++ file1.py
@@ -1,4 +1,13 @@
 def fib(a):
+  """
+  Calculates the Fibonacci number at the specified position.
+
+  Args:
+      a: The position in the Fibonacci sequence for which to calculate the Fibonacci number.
+
+  Returns:
+      The Fibonacci number at the given position.
+  """
   if a <= 1:
     return 1
   return fib(a - 1) + fib(a - 2)
